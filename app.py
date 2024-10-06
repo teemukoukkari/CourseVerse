388aa18579f75f28b6a8c75abeaea5ff3b8f0c92
teemukoukkari <teemu.koukkari@gmail.com>
2024-10-06 19:19:09 +0300

    Add CSRF check for forms

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from os import getenv
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
+csrf = CSRFProtect(app)
 
 import routes
